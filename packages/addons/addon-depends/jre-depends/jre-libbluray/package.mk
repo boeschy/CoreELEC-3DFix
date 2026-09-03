72cc1fec041f99026af2e14b76cfdc9b83b4add0
boeschy <you@example.com>
2026-09-03 12:26:06 +0200

    bumped libbluray to git HEAD

--- packages/addons/addon-depends/jre-depends/jre-libbluray/package.mk
+++ packages/addons/addon-depends/jre-depends/jre-libbluray/package.mk
@@ -13,7 +13,7 @@ PKG_BUILD_FLAGS="-sysroot"
 
 unpack() {
   mkdir -p ${PKG_BUILD}
-  tar --strip-components=1 -xf ${SOURCES}/${PKG_NAME:4}/${PKG_NAME:4}-${PKG_VERSION}.tar.xz -C ${PKG_BUILD}
+  tar --strip-components=1 -xf ${SOURCES}/${PKG_NAME:4}/${PKG_NAME:4}-${PKG_VERSION}.tar.gz -C ${PKG_BUILD}
 }
 
 pre_configure_target() {
